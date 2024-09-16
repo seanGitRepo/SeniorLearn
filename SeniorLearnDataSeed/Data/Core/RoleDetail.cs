b0918c03f397d2b011092ce13364cfa375af970f
Saxon <saxon.cadet@studytafensw.edu.au>
2024-09-16 20:25:54 +1000

    added identity role class

---
+++ SeniorLearnDataSeed/Data/Core/RoleDetail.cs
@@ -0,0 +1,11 @@
+﻿namespace SeniorLearnDataSeed.Data.Core
+{
+    public static class RoleDetail
+    {
+        public const string Role_User_Standard = "Standard";
+        public const string Role_User_Pro = "Pro";
+        public const string Role_User_Hon = "Honourary";
+        public const string Role_Admin = "Admin";
+
+    }
+}
--- SeniorLearnDataSeed/Program.cs
+++ SeniorLearnDataSeed/Program.cs
@@ -15,7 +15,7 @@ namespace SeniorLearnDataSeed
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+            builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddRazorPages();
             var app = builder.Build();
 
